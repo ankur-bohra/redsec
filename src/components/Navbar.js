e73bf2cec855bb5396e88829d6cb94f0d2c66e5f
ankur-bohra <ankurbohra04@gmail.com>
2021-09-06 16:26:06 +0530

    Convert navbar buttons to uppercase

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends Component {
     renderButton(name) {
         return (
             <NavbarButton
-                name={name}
+                name={name.toUpperCase()}
                 active={this.state.activeButton === name}
                 onClick={() => this.handleClick(name)}
             />
