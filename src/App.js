9622cf23c883569ba020d34dcfa037bc5cc8d52e
ankur-bohra <ankurbohra04@gmail.com>
2021-10-08 23:29:36 +0530

    Change title and icon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar.js';
 import Home from './components/Page.js';
 
 function App() {
+  document.title = "RedSec";
   return (
     <div className="App">
       <Navbar/>
